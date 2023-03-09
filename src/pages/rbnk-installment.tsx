f0914f9b0d8029fcca38feead6319c88d1696913
dunkman32 <dunkman032@gmail.com>
2023-03-09 19:30:07 +0400

    feat: rebank calc

---
+++ src/pages/rbnk-installment.tsx
@@ -0,0 +1,15 @@
+const Rebank = () => {
+  return (
+    <iframe
+      style={{
+        width: '100vw',
+        height: '99.4vh',
+      }}
+      title="identomat"
+      src="https://rebank.ge/"
+      allow="camera"
+    />
+  );
+};
+
+export default Rebank;
