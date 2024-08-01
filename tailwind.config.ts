3d6a242ad4d3af105e403b830915032a56dfd5f2
zura nozadze <zura.nozadze@caucasusauto.com>
2024-08-02 01:31:41 +0400

    test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,3 @@
-import type { Config } from 'tailwindcss';
-
 const config = {
   darkMode: ['class'],
   content: [
@@ -97,7 +95,6 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+};
 
 export default config;
